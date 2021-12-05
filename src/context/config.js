10b0d3491e2d1b5fa1cecc2f2b7040d287348f52
mikejguo290 <mikejguo290@gmail.com>
2021-12-05 18:14:44 +0000

    correct config import path.

--- src/context/config.js
+++ src/context/config.js
@@ -1,1 +1,1 @@
-export const SOCKET_URL = '';
+export const SOCKET_URL = 'https://mst-full-stack-dev-test.herokuapp.com/';
--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import socketio from 'socket.io-client';
-import { SOCKET_URL } from 'config';
+import { SOCKET_URL } from './config';
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext(); //create and export the context. 
