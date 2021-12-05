65d733b12099220ba8c7a119cdb75028825f2e23
mikejguo290 <mikejguo290@gmail.com>
2021-12-05 17:52:47 +0000

    provide SocketContext to App's children.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SocketContext, socket} from '../context/socket'
+import Table from './Table';
 
 function App() {
   return (
     <SocketContext.Provider value={socket}>
-      
+      <Table />
     </SocketContext.Provider>
   );
 }
