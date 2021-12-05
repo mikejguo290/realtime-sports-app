34629e95e4d61e143138ddfb3848711f2c9cddb7
mikejguo290 <mikejguo290@gmail.com>
2021-12-05 22:03:42 +0000

    insert a Layout component between App and Table.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {SocketContext, socket} from '../context/socket'
-import Table from './Table';
+import Layout from './Layout';
 
 function App() {
   return (
     <SocketContext.Provider value={socket}>
-      <Table />
+      <Layout />
     </SocketContext.Provider>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Table from './Table';
+
+function Layout() {
+  return (
+    <Table/>
+  );
+}
+
+export default Layout;
