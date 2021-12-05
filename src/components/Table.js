70c08f3a7e15e156d218e57bdc565b2343dc2f37
mikejguo290 <mikejguo290@gmail.com>
2021-12-05 18:52:34 +0000

    set up socket listener on Table for 'data-update' events.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect }from 'react';
+import { SocketContext } from '../context/socket';
 
 export default function Table(){
+
+    const socket = useContext(SocketContext);
+
+    const handleUpdate=(args)=>{ // listener function
+        console.log(args);
+    }
+    
+    useEffect(()=>{
+        // after rendering, add a listener to listen for the event 'data-update'
+        socket.on('data-update', handleUpdate);
+       
+        return ()=>{
+            // before component is unmounted, remove all bound event handlers
+            socket.off('data-update', handleUpdate);
+        }
+    },[socket]);
+    
     return (
-        <div>Hello World</div>
+        <h2>Golfers</h2>
     )
 }
