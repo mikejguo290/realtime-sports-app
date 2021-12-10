d930426adb5e70e4e99ce157aca2d581fa20e079
mikejguo290 <mikejguo290@gmail.com>
2021-12-10 12:15:04 +0000

    comments for socket context.

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import socketio from 'socket.io-client';
+import socketio from 'socket.io-client'; // also import {io} ... const socket = io(url);
 import { SOCKET_URL } from './config';
 
-export const socket = socketio.connect(SOCKET_URL);
+export const socket = socketio.connect(SOCKET_URL); //create a socket instance, which connects to url of event emmitter
 export const SocketContext = React.createContext(); //create and export the context. 
