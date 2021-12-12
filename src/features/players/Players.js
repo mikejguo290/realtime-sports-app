830c2d9af4bc3ab3689c59e60b8684c7bfa6d5e0
mikejguo290 <mikejguo290@gmail.com>
2021-12-12 21:53:46 +0000

    calculate and pass on searchTerm filtered list of players to PlayerTable.

--- src/features/players/Players.js
+++ src/features/players/Players.js
@@ -2,13 +2,23 @@ import React, { useContext, useEffect }from 'react';
 import { SocketContext } from '../../context/socket';
 import PlayerTable from '../../components/PlayerTable'
 import { addPlayer, selectPlayers } from './playersSlice';
+import {  selectSearch } from '../search/searchSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Players(){
-
+    // passes players filtered by search term to PlayerTable.
     const socket = useContext(SocketContext);
     const dispatch = useDispatch(); 
-    const players = useSelector(selectPlayers);    
+    const players = useSelector(selectPlayers);
+    const search = useSelector(selectSearch);
+
+    const displayedPlayers = players.filter( player => {
+        // calculate players filtered by matching to first or last name. startswith.
+        const firstName = player.First.toLowerCase();
+        const lastName = player.Last.toLowerCase();
+        const searchTerm = search.toLowerCase();
+        return firstName.startsWith(searchTerm) || lastName.startsWith(searchTerm);
+    })
     
     useEffect(()=>{
         // after rendering, add a listener to listen for the event 'data-update'
@@ -26,7 +36,7 @@ export default function Players(){
     
     return (
         <>
-            <PlayerTable rows={players} />
+            <PlayerTable rows={displayedPlayers} />
         </>
     )   
 }
