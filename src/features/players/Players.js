a439db6792186f40cc08406b0b3469b866355b48
mikejguo290 <mikejguo290@gmail.com>
2021-12-12 19:25:27 +0000

    use Redux's playerSlice to manage the players state and move logic out of fn component, into reducers.

--- src/features/players/Players.js
+++ src/features/players/Players.js
@@ -1,34 +1,28 @@
-import React, { useContext, useEffect, useState }from 'react';
+import React, { useContext, useEffect }from 'react';
 import { SocketContext } from '../../context/socket';
 import PlayerTable from '../../components/PlayerTable'
+import { addPlayer, selectPlayers } from './playersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 export default function Players(){
 
     const socket = useContext(SocketContext);
-    
-    const [ players, setPlayers ] = useState([]);
-    const handleUpdate=(data)=>{ // listener function
-        // clean the data before saving it to state
-        const playerData = {
-              ...data,
-              id:data.MSTID, // Data grid needs the unique id property to work 
-            }
-        setPlayers((prevState)=>{
-            // conditional to deal with duplicate player ids. 
-            if(!prevState.find(player => player.id === playerData.id)){
-                return [playerData, ...prevState] // new player data is placed on top of rows.
-            }
-        });
-    }
+    const dispatch = useDispatch(); 
+    const players = useSelector(selectPlayers);    
     
     useEffect(()=>{
         // after rendering, add a listener to listen for the event 'data-update'
+        const handleUpdate=(data)=>{ // listener function
+            dispatch(addPlayer(data));
+         }
+
         socket.on('data-update', handleUpdate);
        
         return ()=>{
             // before component is unmounted, remove all bound event handlers
             socket.off('data-update', handleUpdate);
         }
-    },[socket]);
+    },[socket, dispatch]);
     
     return (
         <>
--- src/features/players/playersSlice.js
+++ src/features/players/playersSlice.js
@@ -2,15 +2,31 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const options = {
     name: 'players',
-    initialState: [],
+    initialState: {
+        players:[],
+    },
     reducers: {
         'addPlayer':(state, action)=>{
-            return [action.payload, ...state];
+            const data = action.payload; 
+            // data transformation step - Data grid needs the unique id property to work 
+            const newPlayer = {
+                ...data,
+                id:data.MSTID, 
+              }
+
+            // conditional to deal with duplicate player ids. 
+            if(!state.players.find(player => player.id===newPlayer.id) ){
+                // if not a record with duplicate id, add it.
+                state.players = [newPlayer, ...state.players]
+                // without immer, will have to write two returns, one of which is return state 
+                // the other is return {...state, players: [newPlayer, ...state.players]};
+            }
         }
     }
 }
 
 const playersSlice = createSlice(options);
 
+export const selectPlayers = state => state.players.players;
 export const { addPlayer } = playersSlice.actions;
 export default playersSlice.reducer;
