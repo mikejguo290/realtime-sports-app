1425af9d732c566a1fd58185b6568d1a5a68f674
mikejguo290 <mikejguo290@gmail.com>
2021-12-12 18:28:21 +0000

    Provide Redux store to App's children.

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {SocketContext, socket} from '../context/socket'
 import Layout from '../components/Layout';
+import store from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <SocketContext.Provider value={socket}>
-      <Layout />
+      <Provider store={store}>
+        <Layout />
+      </Provider> 
     </SocketContext.Provider>
   );
 }
